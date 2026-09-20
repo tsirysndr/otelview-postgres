9199b97bb1ad6555df7271f4396f4df3df709e25
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-20 12:26:15 +0300

    Bump version to 0.1.4

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           commonArgs = {
             inherit src;
             pname = "otelview-postgres";
-            version = "0.1.3";
+            version = "0.1.4";
             strictDeps = true;
             nativeBuildInputs = [ pkgs.protobuf ];
             PROTOC = "${pkgs.protobuf}/bin/protoc";
